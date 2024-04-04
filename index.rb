4718722673ab133a090ebea5c6a1f2d504d0ffce
Yusuke0620 <boston19851229@gmail.com>
2024-04-04 10:53:44 +0900

    Progate Ruby

---
+++ index.rb
@@ -0,0 +1,71 @@
+name = "にんじゃわんこ"
+puts name
+
+message = "こんにちは"
+puts message + "にんじゃわんこさん"
+name = "ひつじ仙人さん"
+puts message + name
+
+puts "Progateで" + text
+
+length = 8
+width = 9
+
+puts length + width
+
+
+text = "をマスターしよう"
+
+puts "HTML" + text
+puts "CSS" + text
+puts "Ruby" + text
+
+width = 13
+
+numeber = 2
+puts numeber
+
+numeber =+ 3
+puts numeber
+
+width += 5
+
+puts "面積は#{area}です"
+
+score = 94
+if score > 80
+    puts "よくできました"
+end
+
+score = 80
+
+puts score > 80
+puts score <= 80
+
+if score <= 80 
+    puts "がんばりましょう"
+end
+
+
+
+score = 100
+
+if score == 100
+    puts "満点です"
+end
+
+if score != 100
+    puts "満点ではありません"
+end
+
+score = 96
+
+if score <= 95 && score >= 99
+    puts "高得点です！次は満点を目指しましょう。"
+end
+
+
+number = 48
+
+
+
